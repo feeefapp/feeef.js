7342ca7bf15b1d4793f4ced89d635b06caa21ceb
mohamadlounnas <mohamadlounnas@gmail.com>
2025-10-03 12:36:23 +0100

    Bump version to 0.5.36-dev.5 in package.json. Add random product retrieval method in ProductRepository for enhanced product management.

--- src/feeef/repositories/products.ts
+++ src/feeef/repositories/products.ts
@@ -13,4 +13,16 @@ export class ProductRepository extends ModelRepository<ProductEntity, any, any> 
   constructor(client: AxiosInstance) {
     super('products', client)
   }
+
+  /**
+   * Retrieves a random products from the repository.
+   * @param limit - The number of random products to retrieve. Default is 1.
+   * @returns A promise that resolves to an array of random ProductEntity objects.
+   */
+  async random(limit = 12): Promise<ProductEntity[]> {
+    const response = await this.client.get<ProductEntity[]>(`/products/random`, {
+      params: { limit },
+    })
+    return response.data
+  }
 }
